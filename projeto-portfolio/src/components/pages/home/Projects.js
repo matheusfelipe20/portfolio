0c5a0ee5b1b71e627eceb97d1edc36aa72456af4
Matheus Felipe <felipe_2012matheus@hotmail.com>
2024-11-17 02:11:21 -0300

    ajustando a tela de projetos

--- projeto-portfolio/src/components/pages/home/Projects.js
+++ projeto-portfolio/src/components/pages/home/Projects.js
@@ -9,13 +9,13 @@ import missaoSaude from '../../../img/project/missaoSaude.png';
 import aspnet from '../../../img/project/aspnet.png'
 
 const projectsData = [
-  { id: 1, tag: 'Projeto', title: 'Sist. Apostas Esportivas', color: 'rgb(164 0 255)', imgSrc: apostasEsportivas, link: 'https://github.com/matheusfelipe20/desafio-api-jogos'},
-  { id: 2, tag: 'Projeto', title: 'Proj. DSC - UFPB', color: 'rgb(164 0 255)', imgSrc: meajude, link: 'https://github.com/matheusfelipe20/ChatSocketTCP'},
-  { id: 3, tag: 'Projeto', title: 'Proj. Sua Deita - UFPB', color: 'rgb(164 0 255)', imgSrc: suadieta, link: 'https://github.com/matheusfelipe20/ESA-2022.1--SuaDieta--Projeto-UFPB'},
-  { id: 4, tag: 'Site', title: 'Missão com Saúde', color: 'rgb(255 125 0)', imgSrc: missaoSaude, link: 'https://matheusfelipe20.github.io/Projeto-AS/'},
-  { id: 5, tag: 'Estudo', title: 'Chat Socket TCP', color: '#00a7ff', imgSrc: chatSocketTCP, link: 'https://github.com/matheusfelipe20/ChatSocketTCP'},
-  { id: 6, tag: 'Jogo', title: 'Jogo Nave Simples', color: 'rgb(0 255 86)', imgSrc: jogoNave, link: 'https://github.com/matheusfelipe20/Jogo-Nave-simples-JAVA'},
-  { id: 7, tag: 'Treino', title: 'Proj. Asp.net', color: 'rgb(255 0 57)', imgSrc: aspnet, link: 'https://github.com/matheusfelipe20/Projeto-ASP.NET'},
+  { id: 1, tag: 'Projeto', title: 'Sist. Apostas Esportivas', color: 'var(--color-tag-project)', imgSrc: apostasEsportivas, link: 'https://github.com/matheusfelipe20/desafio-api-jogos', tag_technology: ['Golang', 'JavaScript'],},
+  { id: 2, tag: 'Projeto', title: 'Proj. DSC - UFPB', color: 'var(--color-tag-project)', imgSrc: meajude, link: 'https://github.com/matheusfelipe20/ChatSocketTCP', tag_technology: ['Java', 'Spring Boot'],},
+  { id: 3, tag: 'Projeto', title: 'Proj. Sua Deita - UFPB', color: 'var(--color-tag-project)', imgSrc: suadieta, link: 'https://github.com/matheusfelipe20/ESA-2022.1--SuaDieta--Projeto-UFPB', tag_technology: ['JavaScript', 'HTML/CSS', 'Python'],},
+  { id: 4, tag: 'Site', title: 'Missão com Saúde', color: 'var(--color-tag-site)', imgSrc: missaoSaude, link: 'https://matheusfelipe20.github.io/Projeto-AS/', tag_technology: ['React', 'JavaScript'],},
+  { id: 5, tag: 'Estudo', title: 'Chat Socket TCP', color: 'var(--color-tag-study)', imgSrc: chatSocketTCP, link: 'https://github.com/matheusfelipe20/ChatSocketTCP', tag_technology: ['Java'],},
+  { id: 6, tag: 'Jogo', title: 'Jogo Nave Simples', color: 'var(--color-tag-game)', imgSrc: jogoNave, link: 'https://github.com/matheusfelipe20/Jogo-Nave-simples-JAVA', tag_technology: ['Java'],},
+  { id: 7, tag: 'Treino', title: 'Proj. Asp.net', color: 'var(--color-tag-training)', imgSrc: aspnet, link: 'https://github.com/matheusfelipe20/Projeto-ASP.NET', tag_technology: ['C#', 'ASP.Net', 'HTML/CSS'],},
 ];
 
 const Projects = () => (
@@ -33,6 +33,11 @@ const Projects = () => (
                   [{project.tag}]
                 </span>
                 <h3 className="project-title">{project.title}</h3>
+              </div>
+              <div className="project-category">
+                {project.tag_technology.map((tech, index) => (
+                  <span key={index} className="project-tag-technology">{tech}</span>
+                ))}
               </div>
             </button>
           </a>
